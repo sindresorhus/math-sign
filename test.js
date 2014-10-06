b38abf8e55f6b01e25cb7b3e044eb3fe6119be63
Sindre Sorhus <sindresorhus@gmail.com>
2014-10-06 14:59:01 +0200

    more tests

--- test.js
+++ test.js
@@ -4,12 +4,25 @@ var mathSign = require('./');
 
 test(function (t) {
 	t.assert(mathSign(0) === 0);
+	t.assert(mathSign(100.1) === 1);
 	t.assert(mathSign(-0) === -0);
 	t.assert(mathSign(5) === 1);
 	t.assert(mathSign(-5) === -1);
 	t.assert(mathSign('-5') === -1);
-	t.assert(String(mathSign(NaN)) === 'NaN');
-	t.assert(String(mathSign('foo')) === 'NaN');
-	t.assert(String(mathSign()) === 'NaN');
+	t.assert(isNaN(mathSign(NaN)));
+	t.assert(isNaN(mathSign('foo')));
+	t.assert(isNaN(mathSign()));
+	t.assert(isNaN([1, 1]));
+	t.assert(String(1/mathSign(0)) === 'Infinity');
+	t.assert(String(1/mathSign(-0)) === '-Infinity');
+	t.assert(mathSign([-100.1]) === -1);
+	t.assert(mathSign({toString: function () { return '100'; }}) === 1);
+	t.assert(mathSign({toString: function () { return 100; }}) === 1);
+	t.assert(mathSign({valueOf: function () { return -1.1; }}) === -1);
+	t.assert(mathSign({valueOf: function () { return '-1.1'; }}) === -1);
+	t.assert(mathSign(Infinity) === 1);
+	t.assert(mathSign(-Infinity) === -1);
+	t.assert(mathSign('Infinity') === 1);
+	t.assert(mathSign('-Infinity') === -1);
 	t.end();
 });
