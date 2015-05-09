7134253acd9079bce06ef2c7abd86df604d022aa
Sindre Sorhus <sindresorhus@gmail.com>
2015-05-09 14:05:18 +0200

    tweak package.json keywords

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 module.exports = Math.sign || function (val) {
 	val = Number(val);
 
-	if (val === 0 || /* Number.isNaN() polyfill */ val !== val) {
+	if (val === 0 || /* Number.isNaN() polyfill => */ val !== val) {
 		return val;
 	}
 
