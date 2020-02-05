060672beef79d5598eb203caf9143ad35a90b149
Agastya Chandrakant <me@hanabi.in>
2020-02-05 21:43:10 +0530

    Update test.js to work with ava@3.2.0 + require() import syntax to work without babel

--- test.js
+++ test.js
@@ -1,29 +1,29 @@
-import test from 'ava';
-import numberIsNan from 'number-is-nan';
+const test = require("ava");
+const numberIsNan = require("number-is-nan");
 
 Math.sign = undefined;
-const fn = require('./');
+const fn = require("./");
 
-test(t => {
+test("Testing Math Sign", t => {
 	t.is(fn(0), 0);
 	t.is(fn(100.1), 1);
 	t.is(fn(-0), -0);
 	t.is(fn(5), 1);
 	t.is(fn(-5), -1);
-	t.is(fn('-5'), -1);
+	t.is(fn("-5"), -1);
 	t.true(numberIsNan(fn(NaN)));
-	t.true(numberIsNan(fn('foo')));
+	t.true(numberIsNan(fn("foo")));
 	t.true(numberIsNan(fn()));
 	t.true(numberIsNan(fn([1, 1])));
-	t.is(String(1 / fn(0)), 'Infinity');
-	t.is(String(1 / fn(-0)), '-Infinity');
+	t.is(String(1 / fn(0)), "Infinity");
+	t.is(String(1 / fn(-0)), "-Infinity");
 	t.is(fn([-100.1]), -1);
-	t.is(fn({toString: () => '100'}), 1);
-	t.is(fn({toString: () => 100}), 1);
-	t.is(fn({valueOf: () => -1.1}), -1);
-	t.is(fn({valueOf: () => '-1.1'}), -1);
+	t.is(fn({ toString: () => "100" }), 1);
+	t.is(fn({ toString: () => 100 }), 1);
+	t.is(fn({ valueOf: () => -1.1 }), -1);
+	t.is(fn({ valueOf: () => "-1.1" }), -1);
 	t.is(fn(Infinity), 1);
 	t.is(fn(-Infinity), -1);
-	t.is(fn('Infinity'), 1);
-	t.is(fn('-Infinity'), -1);
+	t.is(fn("Infinity"), 1);
+	t.is(fn("-Infinity"), -1);
 });
